f2a7e8fc36e76fd39bbcd919d3f7afe42abac8ae
kadiri dauda <kadiri.dauda@gmail.com>
2022-09-08 14:45:49 -0700

    update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,16 +2,16 @@
 
 /**
  * main - Entry point
- * Program to print all datatype sizes in mermory of the compute the program run on
+ * Program to print all datatype size
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("Size of a char: %ld bytes(s)\n", sizeof(char));
-	printf("Size of a int:  %ld bytes(s)\n", sizeof(int)); 
-	printf("Size of a long int:  %ld bytes(s)\n", sizeof(long int));
-	printf("Size of a long long int:  %ld bytes(s)\n", sizeof(double));
-	printf("Size of a float:  %ld bytes(s)\n", sizeof(float));
+	printf("Size of a char: %lu bytes(s)\n", sizeof(char));
+	printf("Size of a int:  %lu bytes(s)\n", sizeof(int));
+	printf("Size of a long int:  %lu bytes(s)\n", sizeof(long int));
+	printf("Size of a long long int:  %lu bytes(s)\n", sizeof(long int));
+	printf("Size of a float:  %lu bytes(s)\n", sizeof(float));
 
 	return (0);
 }
