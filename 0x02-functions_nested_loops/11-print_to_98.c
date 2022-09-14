4293302d7f4bebf916f66bc1e955226d49ea7c32
kadiri dauda <kadiri.dauda@gmail.com>
2022-09-14 14:51:29 -0700

    a function that prints all natural numbers from n to 98, followed by a new line.

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - prins all natural numbers from n to 98
+ * followed by a new line
+ * @n: Accepts a int which is used as start point
+ */
+void print_to_98(int n)
+{
+	int a = 0, b;
+
+	if (n <= 98)
+	{
+		while (a <= 98)
+		{
+			if (a != 98)
+			{
+				printf("%d, ", a);
+			}
+			else if (a == 98)
+			{
+				printf("%d\n", a);
+			}
+			a++;
+		}
+	}
+	else if (n >= 98)
+	{
+		for (b = n; b >= 98; b--)
+		{
+			if (j != 98)
+			{
+				printf("%d, ", b);
+			}
+			else if (b == 98)
+			{
+				printf("%d\n", b);
+			}
+		}
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -15,5 +15,9 @@
 7-print_last_digit - A function that prints the last digit of a number
 
 8-8-24_hours - a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
+
 9-time_table - a function that prints the 9 times table, starting with 0.
+
 10-add - a function that adds two integers and returns the result.
+
+11-print_to_98 - a function that prints all natural numbers from n to 98, followed by a new line.
