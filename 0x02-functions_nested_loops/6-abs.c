440c987a168e916821b3d2d1313885cac95c5164
kadiri dauda <kadiri.dauda@gmail.com>
2022-09-14 07:31:41 -0700

    A function the returns the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _abs - return the absolute value of an integer
+ * @n: take an integer arguement
+ * Return: an integer
+ */
+int _abs(int n)
+{
+	return (_putchar(n));
+}
+
