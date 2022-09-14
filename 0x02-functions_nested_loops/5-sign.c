2ece445221f405f2c8029657eabcbaa40ec02f3f
kadiri dauda <kadiri.dauda@gmail.com>
2022-09-14 04:21:08 -0700

    a function that prints the sign of a number.

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - test a number and return
+ * 1 and '+' if n is greater than zero,
+ * '0' and print '0' if n is zero
+ * '-1' and print '-' if n is less than zero
+ * @n: take an integer arguement
+ *
+ * Return : Always 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1, _putchar('+'));
+	}
+	else if (n  == 0)
+	{
+		return (0, _putchar('0'));
+	}
+	else
+	{
+		return (-1, _putchar('-'));
+	}
+}
+
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,4 @@
 0-putchar - a program that prints _putchar, followed by a new line
+1-alphabet - a function that prints the alphabet, in lowercase, followed by a new line.
+2-print_alphabet_x10 - a function that prints 10 times the alphabet, in lowercase, followed by a new line.
+3-islower - a function that checks for lowercase character
