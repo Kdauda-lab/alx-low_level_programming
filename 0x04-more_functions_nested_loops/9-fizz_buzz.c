e232b5474c58f2327fb1c913fc9473d0e9c18df0
kadiri dauda <kadiri.dauda@gmail.com>
2022-09-16 09:56:50 -0700

    a program that prints the numbers from 1 to 100, followed by a new line. But for multiples of three prints Fizz instead of the number and for the multiples of five prints Buzz. For numbers which are multiples of both three and five prints FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include "stdio.h"
+/**
+ * main - a function that prints a square, followed by a new line.
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+
+	int i;
+	char f[] = "Fizz";
+	char b[] = "Buzz";
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("%s%s ", f, b);
+		}
+		else if (i % 3 == 0)
+		{
+			printf("%s ", f);
+		}
+		else if (i % 5 == 0)
+		{
+			printf("%s ", b);
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+	return (0);
+}
+
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -8,3 +8,4 @@
 7-print_diagonal - a function that draws a diagonal line on the terminal.
 7-print_diagonal - a function that draws a diagonal line on the terminal.
 8-print-square - A function that prints a square, followed by a new line.
+9-fizz_buzz - a program that prints the numbers from 1 to 100, followed by a new line. But for multiples of three print Fizz instead of the number and for the multiples of five print Buzz. For numbers which are multiples of both three and five print FizzBuzz
