3ca3522063101c8b79faf0343624bf8442fcebeb
kadiri dauda <kadiri.dauda@gmail.com>
2022-10-09 09:03:16 -0700

    made changes to the code

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -6,7 +6,6 @@
  * str_concat- a function that concatenates two strings.
  * @s1: The first string to concate
  * @s2: The second string to concate
- *
  * Return: A pointer to newstr otherwise NULL on failure
  */
 char *str_concat(char *s1, char *s2)
@@ -19,35 +18,28 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (i = 0; i != '\0'; i++)
-	{
+	len = len1 = 0;
+	for (i = 0; s1[i] != '\0'; i++)
 		len++;
-	}
-	for (j = 0; j != '\0'; j++)
-	{
+	for (i = 0; s2[i] != '\0'; i++)
 		len1++;
-	}
+
 	total_len = len + len1;
 	newstr = (char *)malloc((total_len + 1) * sizeof(char));
 
 	if (newstr == NULL)
 		return (NULL);
-
 	for (i = 0; i < total_len; i++)
 	{
 		if (s1[i] == '\0')
-		{
 			break;
-		}
 		newstr[i] = s1[i];
 	}
 	j = 0;
 	for (; i < total_len; i++)
 	{
 		if (s2[j] == '\0')
-		{
 			break;
-		}
 		newstr[i] = s2[j];
 		j++;
 	}
