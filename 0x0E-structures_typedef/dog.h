71448a937d5c86e66a8f4e731b68b822198ed3f4
kadiri dauda <kadiri.dauda@gmail.com>
2022-10-10 05:33:06 -0700

    a new type struct dog with the following elements

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
-#ifndef MAIN_H
-#define MAIN_H
-struct dog 
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a new type struct dog with
+ * the following elements:
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ */
+struct dog
 {
 	char *name;
 	float age;
