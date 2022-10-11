279af80bd717fd2d830122c93eab5144fbb8ba92
kadiri dauda <kadiri.dauda@gmail.com>
2022-10-11 08:26:16 -0700

    made changes

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,9 +15,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	user = malloc(sizeof(dog_t));
 	if (user == NULL)
-		return(NULL);
+		return (NULL);
 	user->name = name;
 	user->age = age;
 	user->owner = owner;
-	return user;
+	return (user);
 }
