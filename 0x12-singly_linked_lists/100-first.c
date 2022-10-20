98af0e38fc9805b604c79573d20ba475aae87425
kadiri dauda <kadiri.dauda@gmail.com>
2022-10-20 15:09:06 -0700

    made changes to the code

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,6 +6,6 @@ void __attribute__((constructor)) first(void);
  */
 void first(void)
 {
-	printf("you're beat! and yet, you must allow,\n"
+	printf("You're beat! and yet, you must allow,\n"
 			"I bore my house upon my back!\n");
 }
