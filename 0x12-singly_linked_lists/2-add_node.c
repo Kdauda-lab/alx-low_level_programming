83f86c99cc81a525d3ae50e25b701f9baa5aebc1
kadiri dauda <kadiri.dauda@gmail.com>
2022-10-20 14:44:32 -0700

    a function that adds a new node at the beginning of a list_t list.

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,54 +1,28 @@
 #include "lists.h"
 #include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-list_t *new_node(const char *str);
 /**
- * add_node_end - added a new node to the end of a list.
- * @head: pointer to pointer of a list_t
- * @str: string to copy into a node.
+ * add_node - appends a node to the head
+ * @head: address to a node list_t
+ * @str: a string to add to the lists content
  *
- * Return: pointer to a list_t
+ * Return: a pointer to a list_t
  */
-list_t *add_node_end(list_t **head, const char *str)
+list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
-
-	if (!(*head))
-	{
-		*head = new_node(str);
-		return (*head);
-	}
-	if (!(*head)->next)
-	{
-		new = new_node(str);
-		new->next = (*head)->next;
-		(*head)->next = new;
-	}
-	else
-		add_node_end(&(*head)->next, str);
-	return (*head);
-}
-/**
- * new_node - create a new node around a string src.
- * @str: string to add to the node
- *
- * Return: pointer to a list_t
- */
-
-list_t *new_node(const char *str)
-{
-	list_t *new;
-	size_t len;
+	size_t len = 0;
 
 	new = malloc(sizeof(list_t));
 	if (!new)
 		return (NULL);
-	len = 0;
 	while (str[len])
 		len++;
-	new->str = strdup(str);
 	new->len = len;
-	new->next = NULL;
-	return (new);
+	new->str = strdup(str);
+	if (!(*head))
+		new->next = NULL;
+	else
+		new->next = *head;
+	*head = new;
+	return (*head);
 }
